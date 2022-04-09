c3618a3626d5a055c8274ec73c581b821e3523d4
bpelikan <bartoszpelikan@gmail.com>
2022-04-09 19:30:43 +0200

    zadanie4 vm template

--- spotkanie1/zadanie4/vm.tf
+++ spotkanie1/zadanie4/vm.tf
@@ -1,53 +1,53 @@
-# resource "azurerm_virtual_network" "example" {
-#   name                = "example-network"
-#   address_space       = ["10.0.0.0/16"]
-#   location            = azurerm_resource_group.main_rg.location
-#   resource_group_name = azurerm_resource_group.main_rg.name
-# }
+resource "azurerm_virtual_network" "vnet" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.main_rg.location
+  resource_group_name = data.azurerm_resource_group.main_rg.name
+}
 
-# resource "azurerm_subnet" "example" {
-#   name                 = "internal"
-#   resource_group_name  = azurerm_resource_group.main_rg.name
-#   virtual_network_name = azurerm_virtual_network.example.name
-#   address_prefixes     = ["10.0.2.0/24"]
-# }
+resource "azurerm_subnet" "subnet" {
+  name                 = "internal"
+  resource_group_name  = data.azurerm_resource_group.main_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 
-# resource "azurerm_network_interface" "example" {
-#   name                = "example-nic"
-#   location            = azurerm_resource_group.main_rg.location
-#   resource_group_name = azurerm_resource_group.main_rg.name
+resource "azurerm_network_interface" "nic" {
+  name                = "example-nic"
+  location            = data.azurerm_resource_group.main_rg.location
+  resource_group_name = data.azurerm_resource_group.main_rg.name
 
-#   ip_configuration {
-#     name                          = "internal"
-#     subnet_id                     = azurerm_subnet.example.id
-#     private_ip_address_allocation = "Dynamic"
-#   }
-# }
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 
-# resource "azurerm_linux_virtual_machine" "example" {
-#   name                = "example-machine"
-#   resource_group_name = azurerm_resource_group.main_rg.name
-#   location            = azurerm_resource_group.main_rg.location
-#   size                = "Standard_F2"
-#   admin_username      = "adminuser"
-#   network_interface_ids = [
-#     azurerm_network_interface.example.id,
-#   ]
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm"
+  resource_group_name = data.azurerm_resource_group.main_rg.name
+  location            = data.azurerm_resource_group.main_rg.location
+  size                = "Standard_B1ls"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
 
-#   admin_ssh_key {
-#     username   = "adminuser"
-#     public_key = file("~/.ssh/id_rsa.pub")
-#   }
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
 
-#   os_disk {
-#     caching              = "ReadWrite"
-#     storage_account_type = "Standard_LRS"
-#   }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-#   source_image_reference {
-#     publisher = "Canonical"
-#     offer     = "UbuntuServer"
-#     sku       = "16.04-LTS"
-#     version   = "latest"
-#   }
-# }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
