4990be8220bb245717881bb2397957fc650251ed
bpelikan <bartoszpelikan@gmail.com>
2022-07-13 14:31:30 +0200

    zadanie3.1 DNS link to VNET

--- spotkanie3/zadanie1/dns.tf
+++ spotkanie3/zadanie1/dns.tf
@@ -5,10 +5,30 @@ resource "azurerm_private_dns_zone" "privatelink-azurewebsites-net" {
 }
 
 
-# DNS links -> VNET-HUB
+
+
+# DNS link -> VNET-HUB
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet-hub-privatelink-azurewebsites-net-dnszone-virtuallink" {
   name                  = "vnet-hub-privatelink-azurewebsites-net-dnszone-virtuallink"
   resource_group_name   = azurerm_resource_group.netops-prd-dns.name
   private_dns_zone_name = azurerm_private_dns_zone.privatelink-azurewebsites-net.name
   virtual_network_id    = azurerm_virtual_network.vnet-hub.id
+}
+
+
+# DNS link -> VNET-SPOKE-PRD
+resource "azurerm_private_dns_zone_virtual_network_link" "vnet-spoke-prd-privatelink-azurewebsites-net-dnszone-virtuallink" {
+  name                  = "vnet-spoke-prd-privatelink-azurewebsites-net-dnszone-virtuallink"
+  resource_group_name   = azurerm_resource_group.netops-prd-dns.name
+  private_dns_zone_name = azurerm_private_dns_zone.privatelink-azurewebsites-net.name
+  virtual_network_id    = azurerm_virtual_network.vnet-spoke-prd.id
+}
+
+
+# DNS link -> VNET-SPOKE-DEV
+resource "azurerm_private_dns_zone_virtual_network_link" "vnet-spoke-dev-privatelink-azurewebsites-net-dnszone-virtuallink" {
+  name                  = "vnet-spoke-dev-privatelink-azurewebsites-net-dnszone-virtuallink"
+  resource_group_name   = azurerm_resource_group.netops-prd-dns.name
+  private_dns_zone_name = azurerm_private_dns_zone.privatelink-azurewebsites-net.name
+  virtual_network_id    = azurerm_virtual_network.vnet-spoke-dev.id
 }
